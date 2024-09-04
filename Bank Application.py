002a6f3f5075f9ae71a291f35b78300a38d76181
Shaik Allabakash <139256843+allabakash486@users.noreply.github.com>
2024-09-04 20:00:04 +0530

    Update Bank Application.py

--- Bank Application.py
+++ Bank Application.py
@@ -2,18 +2,23 @@ class ourbank():
     nob  = 1
     ifsc = 'OUB102030'
     ROI  = 5
+    
     def __init__(self,name,mob,bal,pin,Aadhar):
         self.name = name
         self.mob  = mob
         self.bal  = bal
         self.pin  = pin
         self.Adr  = Aadhar
+
+    
     # the  below method is used to check the balance 
     def check_bal(self):
         if self.pin==self.takepin():
             print(f'Availabe balance is {self.bal}')
         else:
             print('Invalid pin')
+
+    
     # the  below method is used to Depodite the amount 
     def deposite(self):
         if self.pin==self.takepin():
@@ -23,6 +28,8 @@ class ourbank():
             print(f'Available balance is {self.bal}')
         else:
             print('Invalid pin')
+
+    
     # Hear the method is used to withdraw the amount 
     def withdraw(self):
         if self.pin==self.takepin():
@@ -35,16 +42,22 @@ class ourbank():
                 print('Insuffcient funds')
         else:
             print('Invalid pin')
+
+    
     #Hear you can change the class attributes
     @classmethod
     def croi(self):
         newv = float(input('Enter the new roi'))
         ourbank.ROI = newv
+
+    
     # Pin checking by using stattic method
     @staticmethod
     def takepin():
         pasword =int(input('Enter the pin'))
         return pasword
+        
+#Creating the objetcs using class references
 user1 = ourbank('A',19282838,5000,1111,1029379)
 user2 = ourbank('B',192182838,5000,2222,1029379)
 user2.withdraw()
